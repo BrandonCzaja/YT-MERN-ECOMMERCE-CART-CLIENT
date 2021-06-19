b2d0babb03794a4231afa163a2541e4b0401203a
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-19 17:07:28 -0500

    Added styling to SideDrawer

--- src/Components/SideDrawer.jsx
+++ src/Components/SideDrawer.jsx
@@ -1,13 +1,31 @@
 import "../styles/sideDrawer.css";
+import { Link } from "react-router-dom";
 
-const SideDrawer = ({ show }) => {
+const SideDrawer = ({ show, click }) => {
 	// Dynamic Styling
 	const sideDrawerClass = ["sideDrawer"]; // Set the className to an array
 	if (show) {
 		sideDrawerClass.push("show"); // push style of "show" onto the class styling
 	}
 
-	return <div className={sideDrawerClass.join(" ")}></div>;
+	return (
+		<div className={sideDrawerClass.join(" ")}>
+			<ul className="sideDrawer_links" onClick={click}>
+				<li>
+					<Link to="/cart">
+						<i className="fas fa-shopping-cart"></i>
+						<span>
+							Cart <span className="sideDrawer_cart_icon">0</span>
+						</span>
+					</Link>
+				</li>
+
+				<li>
+					<Link to="/">Shop</Link>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default SideDrawer;
