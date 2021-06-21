c8ea3fdfda29f6be033d7f5f49bb2d480f853250
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-21 12:45:52 -0500

    Finished styling the products and home screens

--- src/Components/Screens/Home.jsx
+++ src/Components/Screens/Home.jsx
@@ -1,7 +1,20 @@
 import "../../styles/home.css";
+import Product from "../Screens/Product.jsx";
 
 const Home = () => {
-	return <div className="home">Home Screen</div>;
+	return (
+		<div className="home">
+			<h2 className="home_screen_title">Latest Products</h2>
+			<div className="home_screen_products">
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
