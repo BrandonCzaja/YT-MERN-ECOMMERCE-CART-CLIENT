a142616bc82a87f69d698af59ae4eb9a2a736155
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-22 14:47:13 -0500

    Updated some styling. My styling is not working the way the tutorial does at the moment

--- src/Components/Screens/Cart.jsx
+++ src/Components/Screens/Cart.jsx
@@ -7,11 +7,14 @@ const Cart = () => {
 			<div className="cart_left">
 				<h2>Shopping Cart</h2>
 				<CartItem />
+				<CartItem />
 			</div>
 			<div className="cart_right">
 				<div className="cart_screen_info">
 					<p>Subtotal: (0) items</p>
 					<p>$499.99</p>
+				</div>
+				<div>
 					<button>Proceed To Checkout</button>
 				</div>
 			</div>
