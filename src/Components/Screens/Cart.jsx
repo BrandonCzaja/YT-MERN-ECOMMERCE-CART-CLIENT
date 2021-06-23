61da0b2a0614a1e03606c0e3a4592d60fc13f283
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-23 15:29:56 -0500

    Cart.jsx can now dynamically render the cart items

--- src/Components/Screens/Cart.jsx
+++ src/Components/Screens/Cart.jsx
@@ -1,13 +1,24 @@
 import "../../styles/cart.css";
 import CartItem from "../CartItem.jsx";
+import { useSelector, useDispatch } from "react-redux"; // Dispatch updates cart size and redispatches the cart
+import { Link } from "react-router-dom";
 
 const Cart = () => {
+	const dispatch = useDispatch();
+	const cart = useSelector((state) => state.cart); // Get cart state
+	const { cartItems } = cart; // Destructor cartItems from cart state
+
 	return (
 		<div className="cart">
 			<div className="cart_left">
 				<h2>Shopping Cart</h2>
-				<CartItem />
-				<CartItem />
+				{cartItems.length === 0 ? (
+					<div>
+						Your cart is empty <Link to="/">Go Back</Link>
+					</div>
+				) : (
+					cartItems.map((item) => <CartItem />)
+				)}
 			</div>
 			<div className="cart_right">
 				<div className="cart_screen_info">
