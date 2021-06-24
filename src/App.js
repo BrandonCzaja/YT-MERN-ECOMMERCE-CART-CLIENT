ee566a3d5156cdbdd1711097f68a430de1bc4acb
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-24 13:03:21 -0500

    Added class name app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// Screens
-import Home from "../src/Components/Screens/Home.jsx";
-import Product from "../src/Components/Screens/Product.jsx";
-import Cart from "../src/Components/Screens/Cart.jsx";
-
 // Components
 import Navbar from "../src/Components/Navbar.jsx";
 import Backdrop from "../src/Components/Backdrop.jsx";
 import SideDrawer from "../src/Components/SideDrawer.jsx";
+
+// Screens
+import Home from "../src/Components/Screens/Home.jsx";
+import Product from "../src/Components/Screens/Product.jsx";
+import Cart from "../src/Components/Screens/Cart.jsx";
 
 function App() {
 	const [sideToggle, setSideToggle] = useState(false);
@@ -20,7 +20,7 @@ function App() {
 			<Navbar click={() => setSideToggle(true)} />
 			<SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
 			<Backdrop show={sideToggle} click={() => setSideToggle(false)} />
-			<main>
+			<main className="app">
 				<Switch>
 					<Route exact path="/" component={Home} />
 					<Route exact path="/product/:id" component={Product} />
