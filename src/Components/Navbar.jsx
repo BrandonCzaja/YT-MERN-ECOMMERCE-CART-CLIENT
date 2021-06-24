31dd229a2a997c8e2cb0af43fd47b1ed7e88f0f1
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-24 10:22:47 -0500

    Fixed the shop icon number bug

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,23 +7,22 @@ const Navbar = ({ click }) => {
 	const { cartItems } = cart;
 
 	const getCartCount = () => {
-		return cartItems.reduce((quantity, item) => quantity + Number(item.quantity), 0);
+		return cartItems.reduce((quantity, item) => Number(item.quantity) + quantity, 0);
 	};
 
 	return (
 		<nav className="navbar">
-			{/* logo */}
 			<div className="navbar_logo">
 				<h2>MERN Shopping Cart</h2>
 			</div>
-			{/* links */}
+
 			<ul className="navbar_links">
 				<li>
 					<Link to="/cart" className="cart_link">
 						<i className="fas fa-shopping-cart"></i>
 						<span>
 							Cart
-							<span className="cart_icon_badge">0</span>
+							<span className="cart_icon_badge">{getCartCount()}</span>
 						</span>
 					</Link>
 				</li>
