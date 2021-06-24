03d4d874a1d90511756b2397178ec555d95ca52e
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-24 10:25:24 -0500

    Cleaned up code to make it more legible

--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -1,16 +1,18 @@
 import "../styles/product_info.css";
 import { Link } from "react-router-dom";
 
-const Product = ({ imageUrl, name, price, description, productId }) => {
+const Product = ({ imageUrl, description, price, name, productId }) => {
 	return (
 		<div className="product">
 			<img src={imageUrl} alt={name} />
+
 			<div className="product_info">
 				<p className="product_name">{name}</p>
-				<p className="product_description">
-					{description.substring(0, 100)}...
-				</p>
+
+				<p className="product_description">{description.substring(0, 100)}...</p>
+
 				<p className="product_price">{price}</p>
+
 				<Link to={`/product/${productId}`} className="product_button">
 					View
 				</Link>
